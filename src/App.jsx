7cdc0e9d4842fddea3232debf39b5f409dd3a9a7
octaveweb <karansproject955@gmail.com>
2026-01-08 16:10:52 +0530

    404 page - Added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Docs from "./pages/Docs"
 import Projects from "./pages/Projects"
+import PageNotFound from "./pages/PageNotFound"
 
 const App = () => {
   return (
@@ -14,6 +15,8 @@ const App = () => {
         <Route path='/portfolio/about' element={<About />} />
         <Route path='/portfolio/docs' element={<Docs />} />
         <Route path='/portfolio/projects' element={<Projects />} />
+        <Route path='/*' element={<PageNotFound />} />
+          
       </Routes>
 
     </>
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <div>
+        <h1
+        style={{
+            color: "#fff",
+            fontSize: "7rem"
+        }}
+      >404 | Page not Found</h1>
+    </div>
+  )
+}
+
+export default PageNotFound
